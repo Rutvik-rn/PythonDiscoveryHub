f728d02ddd3983498e4364bc5259ead7bac1e105
Rutvik N <62597322+Rutvik-rn@users.noreply.github.com>
2024-01-20 14:26:07 +0530

    A1_Highest_Score_Calc.py

--- A1_Highest_Score_Calc.py
+++ A1_Highest_Score_Calc.py
@@ -1,3 +1,9 @@
+"""This Python script demonstrates a simple yet effective way to find the highest score from a list of student scores without relying on built-in functions 
+like max. The code employs a straightforward algorithm that iterates through the list using a for loop, 
+comparing each score with the current highest score found so far. """
+# Sample Input = 150 142 185 120 171 184 149 199
+# Expected Output = 199
+
 # Input a list of student scores
 student_scores = input().split()
 for n in range(0, len(student_scores)):
